2b19e560dc77d6423bc74a713d017574554ff088
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-07-25 19:25:47 -0300

    Product findById

--- spring-boot-ecommerce/src/main/java/io/github/tavodin/ecommerce/controllers/ProductController.java
+++ spring-boot-ecommerce/src/main/java/io/github/tavodin/ecommerce/controllers/ProductController.java
@@ -5,6 +5,7 @@ import io.github.tavodin.ecommerce.services.ProductService;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,12 @@ public class ProductController {
     }
 
     @GetMapping
-    public PagedModel<ProductDTO> findAllPaged(Pageable pageable) {
+    public PagedModel<ProductDTO> findAllProductsPaged(Pageable pageable) {
         return service.findAllProducts(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ProductDTO findByProductId(@PathVariable Long id) {
+        return service.findByProductId(id);
     }
 }
--- spring-boot-ecommerce/src/main/java/io/github/tavodin/ecommerce/services/ProductService.java
+++ spring-boot-ecommerce/src/main/java/io/github/tavodin/ecommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package io.github.tavodin.ecommerce.services;
 
 import io.github.tavodin.ecommerce.dto.ProductDTO;
+import io.github.tavodin.ecommerce.exceptions.ResourceNotFoundException;
 import io.github.tavodin.ecommerce.repositories.ProductRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,5 +20,11 @@ public class ProductService {
     public PagedModel<ProductDTO> findAllProducts(Pageable pageable) {
         Page<ProductDTO> productPaged = repository.findAll(pageable).map(ProductDTO::new);
         return new PagedModel<>(productPaged);
+    }
+
+    public ProductDTO findByProductId(Long id) {
+        return repository.findById(id)
+                .map(ProductDTO::new)
+                .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
     }
 }
