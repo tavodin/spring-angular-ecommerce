dafe377f9e7c8dbb710413eccdca77ded8633542
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-07-26 14:53:12 -0300

    getProductList product service

--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Product } from '../common/product';
+import { PaginatedResponse } from '../interfaces/paginatedreponse.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8080/api/v1/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getProductList(): Observable<Product[]> {
+    return this.httpClient.get<PaginatedResponse<Product>>(this.baseUrl).pipe(
+      map(res => res.content)
+    );
+  }
 }
