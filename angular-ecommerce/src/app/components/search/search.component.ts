e8b747ae759ab33c054cfed06821a32eb2b5ff9d
Gustavo Teixeira <gustavo.teixeira2199@gmail.com>
2025-07-28 17:18:43 -0300

    Sent Data to search route

--- angular-ecommerce/src/app/components/search/search.component.ts
+++ angular-ecommerce/src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class SearchComponent {
 
+  constructor(private router: Router) { }
+
+  doSearch(value: string) {
+    console.log(`value=${value}`);
+    this.router.navigateByUrl(`/search/${value}`);
+  }
 }
